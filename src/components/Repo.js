77d2b7237120bd825b05e4ce6bf03e63e6f1f7dd
Edullidan <128309694+Edullidan@users.noreply.github.com>
2023-07-22 19:27:39 +0400

    comment

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -4,8 +4,9 @@ import styled from "styled-components";
 function Repo({ repositories }) {
   const StyledR = styled.h3`
     padding: 10px;
-    background-color: #0c496b;
+    background: linear-gradient(134deg, #2e2152, #fad6a6);
     color: white;
+    border: 1px solid black;
     margin-bottom: 10px;
     width: 500px;
     border-radius: 20px;
@@ -16,12 +17,30 @@ function Repo({ repositories }) {
     flex-direction: column;
     border: 1px solid black;
     padding: 10px;
-    background-color: #0c496b;
+    background: linear-gradient(134deg, #2e2152, #fad6a6);
     color: white;
     margin-bottom: 10px;
     width: 200px;
     height: 50px;
     border-radius: 20px;
+  `;
+
+  const StyledDiv = styled.div`
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid transparent;
+    padding-bottom: 0px;
+    box-shadow: none;
+    display: block !important;
+    z-index: auto;
+    position: static;
+    background: linear-gradient(120deg, #2e1222, #53353d);
+    height: 500vh;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    opacity: 1;
   `;
 
   const { id } = useParams();
@@ -36,7 +55,7 @@ function Repo({ repositories }) {
   console.log(repositories);
 
   return (
-    <div>
+    <StyledDiv>
       <StyledTitle>Repository Details</StyledTitle>
       {repository && (
         <StyledR>
@@ -45,7 +64,7 @@ function Repo({ repositories }) {
           <p>Repository Description: {repository.description}</p>
         </StyledR>
       )}
-    </div>
+    </StyledDiv>
   );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,17 +6,26 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
   justify-content: center;
   align-items: center;
-  background-color: black;
-  height: 300vh;
-  margin: 0;
-  padding: 0;
-  border-radius: 10px;
+  border-top: 1px solid transparent;
+  padding-bottom: 0px;
+  box-shadow: none;
+  display: block !important;
+  z-index: auto;
+  position: static;
+  background: linear-gradient(120deg, #2e1222, #53353d);
+  height: 500vh;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  opacity: 1;
 `;
 const StyledUl = styled.ul`
-  background-color: #1d1e1e;
+  background: linear-gradient(134deg, #2e2152, #fad6a6);
   margin-left: 50px;
+  font-size: 17px;
   flex-direction: column;
-
+  margin-left: 13px;
   border-radius: 20px;
   margin-top: -150px;
   max-width: 300px;
@@ -46,33 +55,42 @@ const StyledInputContainer = styled.div`
 `;
 
 const StyledInput = styled.input`
-  border-radius: 10px 0px 0px 10px;
-  background-color: #263238;
+  background: linear-gradient(134deg, #2e2152, #fad6a6);
   outline: none;
-  border: none;
+  border: 1px solid #009ee2;
+  border-radius: 10px 0px 0px 10px;
   color: white;
   padding: 10px 20px;
   width: 300px;
   font-size: 16px;
   margin-top: -300px;
+  margin-left: -40px;
 `;
 
 const StyledH1 = styled.h1`
-  margin-left: 790px;
   color: white;
+  border-top: 1px solid transparent;
+  background: linear-gradient(134deg, #2e2152, #53353d);
+  margin-top: -5px;
+  box-shadow: none;
+  display: block !important;
+  z-index: auto;
+  padding-bottom: 10px;
 `;
 
 const SearchButton = styled.button`
-  border: none;
   border-radius: 0px 10px 10px 0px;
+
+  border: 0px;
+  background: linear-gradient(134deg, #2e2152, #13e2da);
   cursor: pointer;
-  color: white;
   font-size: 16px;
-  background-color: #504f4f;
-  padding: 10px 20px;
+  padding: 11px 20px;
   margin-top: -300px;
 `;
 
+/*padding: 10px 20px;
+  margin-top: -300px;*/
 const StyledRa = styled.div`
   margin-top: 200px;
   margin-left: 50px;
@@ -80,25 +98,38 @@ const StyledRa = styled.div`
 `;
 
 const StyledSearch = styled.li`
-  display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10px;
+  border: 1px solid #009ee2;
   padding: 10px;
-  color: black;
+  background: linear-gradient(134deg, #2e2152, #fad6a6);
+  color: white;
   margin-bottom: 10px;
-  width: 100%;
+
+  margin-left: -40px;
   height: 50px;
   border-radius: 4px;
 `;
 
 const StyledRepo = styled.ul`
-  margin-top: -140px;
+  margin-left: 50px;
+  font-size: 17px;
+  flex-direction: column;
+  margin-left: 13px;
+  border-radius: 20px;
+  margin-top: -125px;
+  max-width: 300px;
+  margin-right: 300px;
 `;
 
 function Search({ repositories, setRepositories }) {
   const [search, setSearch] = useState("");
   const [DebounceActive, setDebounceActive] = useState(true);
   const [showResults, setShowResults] = useState(false);
-  const debounceSearchForm = useDebounce(search, 1500);
+  const debounceSearchForm = useDebounce(search, 20);
 
   const handleDebounceSearch = useCallback(async () => {
     if (DebounceActive) {
